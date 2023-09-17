c6d6995d1f8e01abae731d5f4188886a88759a3d
server_42 <devbhattacharya42@gmail.com>
2023-09-17 11:19:29 +0530

    Database linked successfully

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require('express')
+
+const {connection} = require('./db/conn')
+
 const app = express()
 const port = process.env.PORT || 3000
 
 
 app.use(express.json())
 
-app.listen(port , () => {
+app.listen(port , async() => {
+    try{
+        await connection
+        console.log("Connected to DB successfully")
+    }catch(e){
+        console.log(e)
+        console.log("Connection unsuccessfull")
+    }
     console.log(`Server is running at port number ${port}`)
 })
